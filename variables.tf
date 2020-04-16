6196abaf883189ea477c18276e25e50549053f06
Kumaraswamy Vithanala <36370570+kumarvna@users.noreply.github.com>
2020-04-16 18:14:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -75,14 +75,15 @@ variable "netwatcher_name" {
 
 variable "application_name" {
     description = "Please provide your application name"
+    default     = "TestApp"
 }
 
 variable "owner_email" {
     description = "Please provide owner email for this environment"
+    default     = "user@example.com"
 }
 
 variable "environment" {
     description = "Please provide your application environment details here"
+    default     = "Production"
 }
-
-
