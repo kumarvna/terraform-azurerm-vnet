f59c82791c0565b807d06600f36037189a8fef92
kumarvna <36370570+kumarvna@users.noreply.github.com>
2020-05-01 12:26:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ locals {
 }
 
 data "azurerm_resource_group" "rgrp" {
-  count = var.create_network && var.create_resource_group == false ? 1 : 0
+  count = var.create_resource_group == tobool(false) ? 1 : 0
   name = "${var.resource_group_name}"
 }
 
